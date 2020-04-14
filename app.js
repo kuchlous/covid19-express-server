ebd80fc94f782129ebd698ab3f832f543bd340d6
Alok Kuchlous <kuchlous@gmail.com>
2020-04-14 08:19:21 +0000

    If latitude/longitude is specified in the request, sort the results by distance from the point. Also remove the CORS as it is being handled by nginx

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const port = 3000
 const { Client } = require('elasticsearch')
 const client = new Client({ host: 'localhost:9200' })
 
- app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
+// app.use(function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+// });
 
-entitySearch = function(res, cityId, level, menuIds, top_left, bottom_right) {
+entitySearchSortByDistance = function(res, cityId, level, menuIds, latitude, longitude, top_left, bottom_right) {
     if (!menuIds || menuIds.length == 0) {
         res.json({
 		data: [],
@@ -29,12 +29,28 @@ entitySearch = function(res, cityId, level, menuIds, top_left, bottom_right) {
     }
 
     if (top_left.lat && bottom_right.lat) {
-        filter.push({geo_bounding_box:
-            {
-                top_left: top_left,
-                bottom_right: bottom_right
-            },
+        filter.push({
+		geo_bounding_box: {
+			rb_pin: {
+				top_left: top_left,
+				bottom_right: bottom_right
+			},
+		}
         });
+    }
+
+    sort_by = []
+    if (latitude) {
+        sort_by.push({
+	    _geo_distance: {
+                rb_pin: {
+		    lat: latitude,
+		    lon: longitude
+		},
+		order : "asc",
+		distance_type: "plane"
+	    }
+	});
     }
 
     client.search({
@@ -47,6 +63,7 @@ entitySearch = function(res, cityId, level, menuIds, top_left, bottom_right) {
                     filter: filter
                 }
             },
+	    sort: sort_by,
         }
     }).then((body) => {
         this.hits = body.hits.hits
@@ -60,16 +77,16 @@ entitySearch = function(res, cityId, level, menuIds, top_left, bottom_right) {
     })
 }
 
-app.get('/places', (req, res, next) => {
+app.get('/places', (req, res) => {
     let cityId = req.query.cityId;
     let level = req.query.level;
     let menuIds = req.query.menuData;
     let top_left = {lat: req.query.topLeftLat, lon: req.query.topLeftLon}
     let bottom_right = {lat:req.query.bottomRightLat, lon: req.query.bottomRightLon};
-    // latitude = req.query.latitude
-    // longitude = req.query.longitude
+    latitude = req.query.latitude
+    longitude = req.query.longitude
     console.log(res, cityId, level, menuIds, top_left, bottom_right)
-    entitySearch(res, cityId, level, menuIds, top_left, bottom_right)
+    entitySearchSortByDistance(res, cityId, level, menuIds, latitude, longitude, top_left, bottom_right)
     return 0;
 });
 
