56b1130b4bca93b8da2629d789dee730ad651451
Alok Kuchlous <kuchlous@gmail.com>
2020-04-16 09:07:42 +0000

    Removed debug messages and added categoryImpacts endpoint

--- app.js
+++ app.js
@@ -67,7 +67,6 @@ entitySearchSortByDistance = function(res, cityId, level, menuIds, latitude, lon
         }
     }).then((body) => {
         this.hits = body.hits.hits
-        console.log("No results =" + hits.length)
         res.json({
 		data: body.hits.hits.map(d => d["_source"]),
 		success: true,
@@ -117,7 +116,6 @@ categoryCounts = function(res, cityId, menuIds, latitude, longitude, radius, top
                     },
                 }
             });
-	    console.log(filter);
         }
 
         countPromises.push(client.count({
@@ -140,7 +138,74 @@ categoryCounts = function(res, cityId, menuIds, latitude, longitude, radius, top
             success: true,
             message: "Data sent successfully",
         });
-        console.log(results);
+    });
+}
+
+categoryImpacts = function(res, cityId, menuIds, latitude, longitude, radius, top_left, bottom_right) {
+    if (!menuIds || menuIds.length == 0) {
+        res.json({
+		data: [],
+		success: true,
+		message: "Data sent successfully",
+	});
+    }
+
+    // If only one param is sent it comes as a scalar
+    menuIds = (menuIds instanceof Array) ?  menuIds : [menuIds];
+
+    var countPromises = []
+    menuIds.forEach((menuId, index) => {
+        let filter = [{term: {menuId: menuId}}];
+        if (cityId) {
+            filter.push({term: {cityId: cityId}})
+        }
+
+        if (top_left.lat && bottom_right.lat) {
+            filter.push({
+                geo_bounding_box: {
+                    rb_pin: {
+                        top_left: top_left,
+                        bottom_right: bottom_right
+                    },
+                }
+            });
+        }
+
+        if (latitude && longitude && radius) {
+            filter.push({
+                geo_distance: {
+		    distance: radius,
+                    rb_pin: {
+			lat: latitude,
+                        lon: longitude
+                    },
+                }
+            });
+        }
+
+        countPromises.push(client.search({
+            index: "rb_locations",
+            body: {
+                query: {
+                    bool: {
+                        filter: filter
+                    }
+                },
+                aggs: {
+		    impact: { "sum" : { "field" : "impact" } }
+                },
+	    }
+        }))
+    });
+
+    Promise.all(countPromises).then((results) => {
+        let data = [];
+	results.forEach((r, index) => data.push({menuData: menuIds[index], impact: r.aggregations.impact.value}));
+        res.json({
+            data: data,
+            success: true,
+            message: "Data sent successfully",
+        });
     });
 }
 
@@ -152,7 +217,6 @@ app.get('/places', (req, res) => {
     let bottom_right = {lat:req.query.bottomRightLat, lon: req.query.bottomRightLon};
     latitude = req.query.latitude
     longitude = req.query.longitude
-    console.log(res, cityId, level, menuIds, top_left, bottom_right)
     entitySearchSortByDistance(res, cityId, level, menuIds, latitude, longitude, top_left, bottom_right)
     return 0;
 });
@@ -165,8 +229,20 @@ app.get('/categoryCounts', (req, res) => {
     latitude = req.query.latitude
     longitude = req.query.longitude
     radius = req.query.radius
-    console.log(res, cityId, menuIds, top_left, bottom_right)
     categoryCounts(res, cityId, menuIds, latitude, longitude, radius, top_left, bottom_right)
+
+    return 0;
+});
+
+app.get('/categoryImpacts', (req, res) => {
+    let cityId = req.query.cityId;
+    let menuIds = req.query.menuData;
+    let top_left = {lat: req.query.topLeftLat, lon: req.query.topLeftLon}
+    let bottom_right = {lat:req.query.bottomRightLat, lon: req.query.bottomRightLon};
+    latitude = req.query.latitude
+    longitude = req.query.longitude
+    radius = req.query.radius
+    categoryImpacts(res, cityId, menuIds, latitude, longitude, radius, top_left, bottom_right)
 
     return 0;
 });
